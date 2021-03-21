fda4db534f33374eb4cada6131ad40ffd6965499
1florki <florki.business@gmail.com>
2021-03-21 14:09:07 +0100

    Update gltf.js

--- gltf.js
+++ gltf.js
@@ -3,11 +3,12 @@ import {
 } from 'https://threejsfundamentals.org/threejs/resources/threejs/r115/examples/jsm/loaders/GLTFLoader.js';
 
 export class GLTFHelper {
-  constructor(dict) {
+  constructor(dict, doneCallback) {
     this.models = {};
     
     this.toLoad = 0;
     this.loaded = 0;
+    this.done = doneCallback;
     if(dict) this.loadMultiple(dict);
   }
   loadMultiple(dict) {
@@ -26,6 +27,8 @@ export class GLTFHelper {
       if (pos != undefined) model.scene.position.copy(pos);
       if (parent != undefined) parent.add(model.scene);
       if (key != undefined) this.models[key] = model.scene;
+      
+      if(this.done != undefined && this.allLoaded()) this.done(); 
     });
   }
   allLoaded() {
